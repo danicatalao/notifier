5d7f19b279ccb104d57a44bc1ca6d600b358c4f3
dani <catalao.danillo@gmail.com>
2025-02-17 00:51:14 -0300

    feat: validação nos campos json de usuario

--- internal/user/user.go
+++ internal/user/user.go
@@ -8,11 +8,11 @@ const APP_USER_TABLE = "app_user"
 
 type AppUser struct {
 	Id          int64      `json:"id" db:"id"`
-	Name        string     `json:"name" db:"name" validate:"required,max=255"`
+	Name        string     `json:"name" db:"name" validate:"required,max=255,alpha"`
 	Email       string     `json:"email" db:"email" validate:"required,email,max=255"`
-	PhoneNumber *string    `json:"phone_number,omitempty" db:"phone_number" validate:"omitempty,max=15"`
-	Webhook     *string    `json:"webhook,omitempty" db:"webhook"`
-	Active      bool       `json:"active" db:"active"`
+	PhoneNumber *string    `json:"phone_number,omitempty" db:"phone_number" validate:"omitempty,max=15,phone"`
+	Webhook     *string    `json:"webhook,omitempty" db:"webhook" validate:"omitempty,url"`
+	Active      bool       `json:"active" db:"active" validate:"required"`
 	OptOutDate  *time.Time `json:"opt_out_date,omitempty" db:"opt_out_date"`
 	CreatedAt   time.Time  `json:"created_at" db:"created_at"`
 	UpdatedAt   time.Time  `json:"updated_at" db:"updated_at"`
